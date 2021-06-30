72f18776ae6906b703b8de46e540609d3ef0ddab
Kevin Stradtman <kevin.stradtman@gmail.com>
2021-06-30 13:54:23 -0400

    Initial commit

---
+++ src/main/java/com/stradtkt/homeatlast/HomeAtLastApplication.java
@@ -0,0 +1,13 @@
+package com.stradtkt.homeatlast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAtLastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeAtLastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stradtkt/homeatlast/HomeAtLastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stradtkt.homeatlast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAtLastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
